aa8360beac8b4e37058cce33327ea52220a447ee
AvenNyan <109649930+AvenNyan@users.noreply.github.com>
2025-08-12 14:36:39 +0300

    v5.0 Create items.vases.js

---
+++ data/items.vases.js
@@ -0,0 +1,23 @@
+// data/items.vases.js
+window.itemsData = {
+  "v100": {
+    name: "Ваза минимал",
+    description: "Современная ваза, высота ~18 см.",
+    group: "Вазы",
+    ozon: "",
+    contacts: { telegram: "@AvenNyan", whatsapp: "+79818522194" },
+    colors: [
+      { code: "W01", img: "images/v100-W01.jpg" },
+      { code: "W02", img: "images/v100-W02.jpg" }
+    ]
+  },
+  "v101": {
+    name: "Ваза гео",
+    description: "Геометрическая ваза для декора.",
+    group: "Вазы",
+    colors: [
+      { code: "W01", img: "images/v101-W01.jpg" },
+      { code: "W02", img: "images/v101-W02.jpg" }
+    ]
+  }
+};
