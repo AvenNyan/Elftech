5e73cc7226ff85d6d28cc1ee9174b1aacb5d178d
AvenNyan <109649930+AvenNyan@users.noreply.github.com>
2025-08-12 14:37:16 +0300

    v5.0 Create items.resin.js

---
+++ data/items.resin.js
@@ -0,0 +1,13 @@
+// data/items.resin.js
+window.itemsData = {
+  "s001": {
+    name: "Фигурка фотополимер",
+    description: "Индивидуальные фигурки на заказ. Только под заказ.",
+    group: "Фигурки (фотополимер)",
+    ozon: "",
+    contacts: { telegram: "@AvenNyan", whatsapp: "+79818522194" },
+    colors: [
+      { code: "S01", img: "images/s001-S01.jpg" }
+    ]
+  }
+};
