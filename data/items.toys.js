8bdd09edbea2f7ff677245b29c035f1b03b9f493
AvenNyan <109649930+AvenNyan@users.noreply.github.com>
2025-08-12 14:56:18 +0300

    v5.0.1

--- data/items.toys.js
+++ data/items.toys.js
@@ -7,9 +7,25 @@ window.itemsData = {
     ozon: "https://ozon.ru/product/t010",
     contacts: { telegram: "@AvenNyan", whatsapp: "+79818522194" },
     colors: [
-      { code: "G01", img: "images/t010-G01.jpg", ozon: "https://ozon.ru/product/t010?color=G01" },
-      { code: "G02", img: "images/t010-G02.jpg", ozon: "" },
-      { code: "G03", img: "images/t010-G03.jpg", ozon: "" }
+      { code: "B01", img: "images/t010-B01.jpg", ozon: "https://ozon.ru/product/t010?color=G01" },
+      { code: "B04", img: "images/t010-B04.jpg", ozon: "" },
+      { code: "B12", img: "images/t010-B12.jpg", ozon: "" },
+      { code: "B14", img: "images/t010-B14.jpg", ozon: "" },
+      { code: "G01", img: "images/t010-G01.jpg", ozon: "" },
+      { code: "G04", img: "images/t010-G04.jpg", ozon: "" },
+      { code: "G05", img: "images/t010-G05.jpg", ozon: "" },
+      { code: "M01", img: "images/t010-M01.jpg", ozon: "" },
+      { code: "M02", img: "images/t010-M02.jpg", ozon: "" },
+      { code: "M03", img: "images/t010-M03.jpg", ozon: "" },
+      { code: "M08", img: "images/t010-M08.jpg", ozon: "" },
+      { code: "R01", img: "images/t010-R01.jpg", ozon: "" },
+      { code: "S01", img: "images/t010-S01.jpg", ozon: "" },
+      { code: "S02", img: "images/t010-S02.jpg", ozon: "" },
+      { code: "S03", img: "images/t010-S03.jpg", ozon: "" },
+      { code: "S04", img: "images/t010-S04.jpg", ozon: "" },
+      { code: "W01", img: "images/t010-W01.jpg", ozon: "" },
+      { code: "Y01", img: "images/t010-Y01.jpg", ozon: "" },
+      { code: "Y02", img: "images/t010-Y02.jpg", ozon: "" }
     ]
   },
   "t011-m": {
@@ -19,21 +35,38 @@ window.itemsData = {
     ozon: "",
     contacts: { telegram: "@AvenNyan", whatsapp: "+79818522194" },
     colors: [
-      { code: "G01", img: "images/t011-m-G01.jpg" },
-      { code: "G02", img: "images/t011-m-G02.jpg" },
-      { code: "G03", img: "images/t011-m-G03.jpg" }
+      { code: "B01", img: "images/t010-m-B01.jpg", ozon: "https://ozon.ru/product/t010?color=G01" },
+      { code: "B04", img: "images/t010-m-B04.jpg", ozon: "" },
+      { code: "B12", img: "images/t010-m-B12.jpg", ozon: "" },
+      { code: "B14", img: "images/t010-m-B14.jpg", ozon: "" },
+      { code: "G01", img: "images/t010-m-G01.jpg", ozon: "" },
+      { code: "G04", img: "images/t010-m-G04.jpg", ozon: "" },
+      { code: "G05", img: "images/t010-m-G05.jpg", ozon: "" },
+      { code: "M01", img: "images/t010-m-M01.jpg", ozon: "" },
+      { code: "M02", img: "images/t010-m-M02.jpg", ozon: "" },
+      { code: "M03", img: "images/t010-m-M03.jpg", ozon: "" },
+      { code: "M08", img: "images/t010-m-M08.jpg", ozon: "" },
+      { code: "R01", img: "images/t010-m-R01.jpg", ozon: "" },
+      { code: "S01", img: "images/t010-m-S01.jpg", ozon: "" },
+      { code: "S02", img: "images/t010-m-S02.jpg", ozon: "" },
+      { code: "S03", img: "images/t010-m-S03.jpg", ozon: "" },
+      { code: "S04", img: "images/t010-m-S04.jpg", ozon: "" },
+      { code: "W01", img: "images/t010-m-W01.jpg", ozon: "" },
+      { code: "Y01", img: "images/t010-m-Y01.jpg", ozon: "" },
+      { code: "Y02", img: "images/t010-m-Y02.jpg", ozon: "" }
     ]
   },
-  "d001": {
-    name: "Дракон малый",
+  "T020": {
+    name: "Дракон большой",
     description: "Дракон с детальной текстурой.",
     group: "Драконы",
     ozon: "https://ozon.ru/product/d001",
     contacts: { telegram: "@AvenNyan", whatsapp: "+79818522194" },
     colors: [
-      { code: "R01", img: "images/d001-R01.jpg", ozon: "https://ozon.ru/product/d001?color=R01" },
-      { code: "B01", img: "images/d001-B01.jpg" },
-      { code: "K01", img: "images/d001-K01.jpg" }
+      { code: "G01", img: "images/T020-G01.jpg", ozon: "https://ozon.ru/product/d001?color=R01" },
+      { code: "M01", img: "images/T020-M01.jpg" },
+      { code: "M02", img: "images/T020-M02.jpg" },
+      { code: "M03", img: "images/T020-M03.jpg" }
     ]
   }
   // при необходимости добавляй новые ключи по шаблону: "t012": { ... }
