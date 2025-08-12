ec9f4aabe80d754a917bddee3f980ea44d71b678
AvenNyan <109649930+AvenNyan@users.noreply.github.com>
2025-08-12 14:30:26 +0300

    v5.0 Create items.palette.js

---
+++ data/items.palette.js
@@ -0,0 +1,7 @@
+// data/items.palette.js
+window.itemsData = {
+  "pal-G01": { img: "images/pal-G01.jpg", code: "G01" },
+  "pal-G02": { img: "images/pal-G02.jpg", code: "G02" },
+  "pal-G03": { img: "images/pal-G03.jpg", code: "G03" },
+  // ...
+};
