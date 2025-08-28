fbeceabd8b0a0b78cd4a52ae4c2c70882b92f5fd
AvenNyan <109649930+AvenNyan@users.noreply.github.com>
2025-08-28 10:08:45 +0300

    Update items.palette.js

--- data/items.palette.js
+++ data/items.palette.js
@@ -1,8 +1,8 @@
 // data/items.palette.js
 window.itemsData = {
-  "G01": { img: "images/G01.jpg", code: "G01" },
-  "G02": { img: "images/G02.jpg", code: "G02" },
-  "G03": { img: "images/G03.jpg", code: "G03" },
-  "W01": { img: "images/W01.jpg", code: "W01" },
-  "R01": { img: "images/R01.jpg", code: "R01" }
+  "G01": { img: "images/G01.jpg", code: "G01", name: "Мятный", material: "PLA" },
+  "G02": { img: "images/G02.jpg", code: "G02", name: "Тёмно-зелёный", material: "PETG" },
+  "G03": { img: "images/G03.jpg", code: "G03", name: "Оливковый", material: "PLA" },
+  "W01": { img: "images/W01.jpg", code: "W01", name: "Белый", material: "PLA" },
+  "R01": { img: "images/R01.jpg", code: "R01", name: "Ярко-красный", material: "PETG" }
 };
