59eb3a63e7bad5cd98c4151393fc0371961343e3
AvenNyan <109649930+AvenNyan@users.noreply.github.com>
2025-08-28 16:11:03 +0300

    Update items.palette.js

--- data/items.palette.js
+++ data/items.palette.js
@@ -1,5 +1,7 @@
-window.itemsData = {
-  "G01": { code: "G01", name: "Мятный", img: "images/G01.jpg", material: "PLA", type: "solid", hex: "#7ED48A", description: "PLA, декор" },
-  "MUL01": { code: "MUL01", name: "Ocean mix", img: "images/MUL01.jpg", material: "PLA", type: "multi", description: "Многоцветная нить" },
-  "T101": { code: "T101", name: "PET-CF", img: "images/T101.jpg", material: "PET-CF", type: "tech", description: "Усиленный карбоном" }
-};
+// data/items.palette.js
+window.paletteData = window.paletteData || {};
+Object.assign(window.paletteData, {
+  "G01": { code:"G01", name:"Мятный", img:"images/G01.jpg", material:"PLA", type:"solid", hex:"#7ed48a", description:"PLA, матовый" },
+  "MUL01": { code:"MUL01", name:"Ocean Mix", img:"images/MUL01.jpg", material:"PLA", type:"multi", description:"Многоцветная нить" },
+  "T101": { code:"T101", name:"PET-CF", img:"images/T101.jpg", material:"PET-CF", type:"tech", description:"Усиленный карбоном" }
+});
