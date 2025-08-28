aa4738793e6d6bc07bf3e407b674e4b5ef68e5e1
AvenNyan <109649930+AvenNyan@users.noreply.github.com>
2025-08-28 09:59:02 +0300

    Update items.palette.js

--- data/items.palette.js
+++ data/items.palette.js
@@ -1,8 +1,8 @@
 // data/items.palette.js
 window.itemsData = {
-  "pal-G01": { img: "images/pal-G01.jpg", code: "G01" },
-  "pal-G02": { img: "images/pal-G02.jpg", code: "G02" },
-  "pal-G03": { img: "images/pal-G03.jpg", code: "G03" },
-  "pal-W01": { img: "images/pal-W01.jpg", code: "W01" },
-  "pal-R01": { img: "images/pal-R01.jpg", code: "R01" }
+  "pal-G01": { img: "images/G01.jpg", code: "G01" },
+  "pal-G02": { img: "images/G02.jpg", code: "G02" },
+  "pal-G03": { img: "images/G03.jpg", code: "G03" },
+  "pal-W01": { img: "images/W01.jpg", code: "W01" },
+  "pal-R01": { img: "images/R01.jpg", code: "R01" }
 };
