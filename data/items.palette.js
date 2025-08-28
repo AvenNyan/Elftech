07b0a85dc6cc893b143fc7d2d00a6e5ca96eda5c
AvenNyan <109649930+AvenNyan@users.noreply.github.com>
2025-08-28 10:22:58 +0300

    Update items.palette.js

--- data/items.palette.js
+++ data/items.palette.js
@@ -1,8 +1,39 @@
 // data/items.palette.js
 window.itemsData = {
-  "G01": { img: "images/G01.jpg", code: "G01", name: "Мятный", material: "PLA" },
-  "G02": { img: "images/G02.jpg", code: "G02", name: "Тёмно-зелёный", material: "PETG" },
-  "G03": { img: "images/G03.jpg", code: "G03", name: "Оливковый", material: "PLA" },
-  "W01": { img: "images/W01.jpg", code: "W01", name: "Белый", material: "PLA" },
-  "R01": { img: "images/R01.jpg", code: "R01", name: "Ярко-красный", material: "PETG" }
+  "G01": {
+    name: "Мятный",
+    group: "Палитра",
+    // миниатюра будет взята из первого цвета
+    colors: [
+      { code: "G01", img: "images/G01.jpg", ozon: "" }
+    ]
+  },
+  "G02": {
+    name: "Тёмно-зелёный",
+    group: "Палитра",
+    colors: [
+      { code: "G02", img: "images/G02.jpg", ozon: "" }
+    ]
+  },
+  "G03": {
+    name: "Оливковый",
+    group: "Палитра",
+    colors: [
+      { code: "G03", img: "images/G03.jpg", ozon: "" }
+    ]
+  },
+  "W01": {
+    name: "Белый",
+    group: "Палитра",
+    colors: [
+      { code: "W01", img: "images/W01.jpg", ozon: "" }
+    ]
+  },
+  "R01": {
+    name: "Ярко-красный",
+    group: "Палитра",
+    colors: [
+      { code: "R01", img: "images/R01.jpg", ozon: "" }
+    ]
+  }
 };
